bd7d3ec742ed9d9fd5b3338f21ebd311d26b9a03
ac85 <ac85@me.com>
2015-11-24 10:13:50 +0100

    Erstellung auf der Seite Aktuelles: ältere news 30/70 Aufteilung

--- wordpress/wp-content/themes/Havixbeck/home.php
+++ wordpress/wp-content/themes/Havixbeck/home.php
@@ -1,17 +1,26 @@
 <?php get_header(); ?>
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<?php
 
-
+$i = 0;
+if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	<div class="row">
 
 
-		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 latest-post">
+		<?php if ($i === 0): ?>
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 latest-post">
+		<?php else: ?>
+			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+		<?php endif; ?>
 			<?php
 			if ( has_post_thumbnail() ) {the_post_thumbnail();}
 			?>
 		</div>
-		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+		<?php if ($i++ === 0): ?>
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+		<?php else: ?>
+			<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+		<?php endif; ?>
 
 			<div class="text-right">
 			<?php the_time('d.m.Y') ?>
@@ -26,7 +35,7 @@
 		</div>
 	</div><!-- row -->
 
-	<hr >
+	<hr>
 <?php endwhile; else: ?>
 	<p><?php _e('Das tut uns Leid.Bis jetzt wurden noch keine Beiträge veröffentlicht.'); ?></p>
 <?php endif; ?>
