d161921994a34c0bf9222d6167f8ec27e6cf38ea
Brian Stark <brian.joe.stark@gmail.com>
2015-12-15 19:43:21 +0100

    DB export

--- wordpress/wp-content/themes/Havixbeck/templates/startseite.php
+++ wordpress/wp-content/themes/Havixbeck/templates/startseite.php
@@ -1,6 +1,15 @@
 
 
+</div>
+<div class="container-fluid full-width-image-align">
+    <div class="full-width-image-on-top">
+        <img src="<?php echo get_template_directory_uri();?>/assets/images/on-top-image.jpg" >
+
+    </div>
+    <div class="breadcrumbs"> <?php if (function_exists('nav_breadcrumb')) nav_breadcrumb(); ?> </div>
+</div><!-- container-fluid -->
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
 
     <?php the_content(); ?>
 
