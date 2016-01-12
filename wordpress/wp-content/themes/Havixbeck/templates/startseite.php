cbed723e34ad4a813133d241ba001914ad90c1bf
Brian Stark <brian.joe.stark@gmail.com>
2016-01-12 12:56:41 +0100

    Startseite, ne Alex ;)

--- wordpress/wp-content/themes/Havixbeck/templates/startseite.php
+++ wordpress/wp-content/themes/Havixbeck/templates/startseite.php
@@ -1,18 +1,45 @@
 <div class="container-fluid full-width-image-align">
-    <div class="full-width-image-on-top">
-        <img src="<?php echo get_template_directory_uri();?>/assets/images/on-top-image.jpg" >
+    <div class="full-width-image-on-top-start">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/startseite-bild.jpg">
 
     </div>
     <div class="breadcrumbs"> <?php if (function_exists('nav_breadcrumb')) nav_breadcrumb(); ?> </div>
 </div><!-- container-fluid -->
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<div class="container-fluid main-padding">
+    <div class="row">
+
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 startseite-link-to-page">
+            <a href=""><img src="<?php echo get_template_directory_uri(); ?>/assets/images/startseite-bild.jpg">
+
+                <div class="grey-overlay"><h4>Angebot</h4></div></a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 startseite-link-to-page secondImg">
+            <a href=""><img src="<?php echo get_template_directory_uri(); ?>/assets/images/startseite-bild.jpg">
+
+                <div class="grey-overlay"><h4>Angebot</h4></div></a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 startseite-link-to-page thirdImg">
+            <a href=""><img src="<?php echo get_template_directory_uri(); ?>/assets/images/startseite-bild.jpg">
+
+                <div class="grey-overlay"><h4>Angebot</h4></div></a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 startseite-link-to-page">
+            <a href=""><img src="<?php echo get_template_directory_uri(); ?>/assets/images/startseite-bild.jpg">
+
+                <div class="grey-overlay"><h4>Angebot</h4></div></a>
+        </div>
 
 
-    <?php the_content(); ?>
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-<?php endwhile; else: ?>
-    <p><?php _e('Sorry, this page does not exist.'); ?></p>
-<?php endif; ?>
+
+            <?php the_content(); ?>
+
+        <?php endwhile;
+        else: ?>
+            <p><?php _e('Sorry, this page does not exist.'); ?></p>
+        <?php endif; ?>
+    </div>
 
 
 
