f5c511fae9c435114d48d9da6683a84756b20368
freddy4489 <frederik_berner@web.de>
2016-01-14 13:26:38 +0100

    header fix

--- wordpress/wp-content/themes/Havixbeck/header.php
+++ wordpress/wp-content/themes/Havixbeck/header.php
@@ -19,7 +19,7 @@
   
 </head>
 <body>
-  <nav class="navbar navbar-default">
+  <nav class="navbar navbar-default" >
     <div class="container-fluid">
       <!-- Brand and toggle get grouped for better mobile display -->
       <div class="navbar-header">
@@ -35,7 +35,7 @@
       </div>
 
       <!-- Collect the nav links, forms, and other content for toggling -->
-      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1" data-spy="affix" data-offset-top="82">
   <?php
         wp_nav_menu(array(
                 'menu' => 'primary',
@@ -49,6 +49,8 @@
                 'walker' => new wp_bootstrap_navwalker())
         );
         ?>
+
+
 
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
