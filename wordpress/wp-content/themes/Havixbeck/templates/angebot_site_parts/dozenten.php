24ac6d6de47a3338164630a881f5f6b592a3803f
freddy4489 <frederik_berner@web.de>
2016-01-21 13:17:23 +0100

    css und dozent

--- wordpress/wp-content/themes/Havixbeck/templates/angebot_site_parts/dozenten.php
+++ wordpress/wp-content/themes/Havixbeck/templates/angebot_site_parts/dozenten.php
@@ -11,26 +11,29 @@ while ($loop->have_posts()) : $loop->the_post();
     $substring = substr($name, 0, 1);
 
     $currentLetter = $substring;
-
+if ($oldLetter != $currentLetter) {
     ?>
-    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 instrument_cat letter">
+    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 instrument_cat letter lettermargin">
         <?php
-
-        if ($oldLetter != $currentLetter) {
-
             echo $currentLetter;
-
-
             $oldLetter = $currentLetter;
-
-        }
         ?>
-
     </div>
-    <?php ?>
-
+    <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9 someSpace lettermargin">
+        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><br>
+    </div>
+    <?php }
+    else{
+       
+    ?>
+    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 instrument_cat letter"></div>
     <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9 someSpace">
         <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><br>
     </div>
+    <?php
+        }
+    ?>
+    
+
 
 <?php endwhile; ?>
